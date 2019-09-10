de9da28bf1ee2f744348d938dee6ac0ccb71bcd3
Kunal Kanere <kunalkanere@gmail.com>
2019-09-10 22:03:06 +1000

    first props and children

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      //cannot use class as it is reserved word in JS
+      // all below tags are JSX which are compiled into HTML
+      <div className="App"> 
+        <h1>Hello React World</h1>
+        <p>This is my first react page!!</p>
+        <Person name="Kunal" age="31" />
+        <Person name="Karen" age="25"/>
+        <Person name="Mayur" age="35" >I am the eldest!!</Person>
       </div>
     );
+    // React compiles to below code internally
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1',null,'Some text here'));
   }
 }
 
