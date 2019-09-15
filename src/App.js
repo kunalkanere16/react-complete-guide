d9639416836464e95dfa4a16b4b4db82c4582cb3
Kunal Kanere <kunalkanere@gmail.com>
2019-09-15 19:41:36 +1000

    added css, listen to event, pass method reference, pass method arguments

--- src/App.js
+++ src/App.js
@@ -13,28 +13,57 @@ class App extends Component {
     profession: 'something'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('was clicked!!');
     //DON'T DO THIS: this.state.persons[0].name = 'Kanere';
     this.setState({
       persons : [
-        {name : 'Karen Mansukhani', age : 18},
+        {name : newName, age : 18},
         {name : 'Kunal', age : 31},
         {name : 'Mayur', age : 32}
       ]
     });
   }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons : [
+        {name : 'Karen', age : 18},
+        {name : event.target.value, age : 31},
+        {name : 'Mayur', age : 32}
+      ]
+    });
+  }
+
   render() {
+    const style =  {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     return (
       //cannot use class as it is reserved word in JS
       // all below tags are JSX which are compiled into HTML
       <div className="App"> 
         <h1>Hello React World</h1>
         <p>This is my first react page!!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} >I am the eldest!!</Person>
+        {/*try not to use below style to call method with args */}
+        <button 
+          style={style}
+          onClick={ () => this.switchNameHandler('Karen KK')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Mrs Karen Kanere')}
+          changed={this.nameChangedHandler} />
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} >I am the eldest!!</Person>
       </div>
     );
     // React compiles to below code internally
