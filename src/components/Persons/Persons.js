0a8cfecebfccf39dca6158a43c23b1fd545e8001
Kunal Kanere <kunalkanere@gmail.com>
2019-10-31 22:48:16 +1100

    PureComponents instead of shouldComponentUpdate, hoc Aux component

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
+import React, { PureComponent} from 'react'
 import Person from './Person/Person'
 
-class Persons extends Component{
+class Persons extends PureComponent{
     // static getDerivedStateFromProps(props, state){
     //     console.log('[Persons.js] getDerivedStateFromProps');
     //     return state;
@@ -12,15 +12,15 @@ class Persons extends Component{
     //     console.log('[Persons.js] componentWillReceiveProps', props);
     // }
 
-    shouldComponentUpdate(nextProps, nextState){
-        console.log('[Persons.js] shouldComponentUpdate');
-        //reference pointers are compared
-        if(nextProps.persons !== this.props.persons){
-            return true;
-        }else{
-            return false;
-        }
-    }
+    // shouldComponentUpdate(nextProps, nextState){
+    //     console.log('[Persons.js] shouldComponentUpdate');
+    //     //reference pointers are compared
+    //     if(nextProps.persons !== this.props.persons){
+    //         return true;
+    //     }else{
+    //         return false;
+    //     }
+    // }
 
     getSnapshotBeforeUpdate(prevProps, prevState){
         console.log('[Persons.js] getSnapshotBeforeUpdate');
